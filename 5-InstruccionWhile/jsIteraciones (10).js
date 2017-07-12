27aec0502cf1a093e8e8be3503ebb29380930bc5
itsjustfede <fede00skyler@gmail.com>
2017-07-12 12:20:47 -0300

    Resolvi el ejercicio 10 de la instrucción While y me faltó terminar el TP FerreteIluminacion

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,12 +3,16 @@ function Mostrar()
 var contador0 = 0;
 var contadorpos = 0;
 var contadorneg = 0;
+var contadorpares = 0;
 var acumuladorneg = 0;
 var acumuladorpos = 0;
 var acumuladorsuma = 0;
 var acumuladormulti = 1;
 var num;
+var resultadoneg;
+var resultadopos;
 var respuesta = true;
+var dif;
 
 while (respuesta == true) {
 	num = prompt("Ingrese un número");
@@ -17,19 +21,47 @@ while (respuesta == true) {
 		num = prompt("Ingrese un verdadero número");
 		num = parseInt(num);
 	}	
-	if (num < 0){
+	if (num < 0){ //cuenta los numeros negativos
 		acumuladorneg += num;
 		contadorneg ++;
 		}
-	else
-	if (num > 0){
+	else if (num > 0){ //cuenta los numeros positivos
 		acumuladorpos += num;
 		contadorpos ++;
 	}
-	else{
+	else{ // cuenta los ceros
 		contador0 ++;
 	}
-	respuesta = confirm("¿Queres seguir agregando números?")
+	if (num % 2 == 0 ){ // cuenta los pares
+		contadorpares ++;
+	}
+
+	dif = contadorneg - contadorpos;
+	if (dif < 0){
+		dif = dif * -1;
+		}
+	if (contadorneg > 0) {
+		resultadoneg = acumuladorneg / contadorneg; //hay numeros negativos los cuenta
+	}
+	else{
+		resultadoneg = "No hay numeros negativos"; // si no avisa
+	}
+	if (contadorpos > 0) {
+		resultadopos = acumuladorpos / contadorpos;
+	}
+	else{
+		resultadopos = "No hay numeros positivos";
+	}
+respuesta = confirm("¿Queres seguir agregando números?")
 }
-document.write("1) " + acumuladorneg + " 2) " + acumuladorneg + " 3) ");
+
+document.write("1) " + acumuladorneg + "<br>");
+document.write("2) " + acumuladorpos + "<br>");
+document.write("3) " + contadorpos + "<br>");
+document.write("4) " + contadorneg + "<br>");
+document.write("5) " + contador0 + "<br>");
+document.write("6) " + contadorpares + "<br>");
+document.write("7) " + resultadopos + "<br>");
+document.write("8) " + resultadoneg +  "<br>");
+document.write("9) " + dif);
 }
--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,5 +1,5 @@
 /*4.	Para el departamento de iluminación:
-Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $precio pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
@@ -10,5 +10,59 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+var cantidad;
+var marca;
+var precio = 35;
+var descuento;
+var preciofinal;
+
+cantidad = document.getElementById("Cantidad").value;
+marca = document.getElementById("Marca").value;
+
+cantidad = parseInt(cantidad);
+descuento = parseInt(descuento);
+precio = parseInt(precio);
+preciofinal = parseInt(preciofinal);
+
+if (cantidad >= 6){
+        descuento = (cantidad * precio) * 0.50;
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad == 5 && marca == "ArgentinaLuz"){
+        descuento = (cantidad * precio) * 0.40; 
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad == 5 && marca != "ArgentinaLuz") {
+        descuento = (cantidad * precio) * 0.30;
+        preciofinal = (cantidad * precio) - descuento; 
+    }
+else if (cantidad == 4 && marca == "ArgentinaLuz" || cantidad == 4 && marca == "FelipeLamparas"){
+        descuento = (cantidad * precio) * 0.25;
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad == 4 && marca != "ArgentinaLuz" || cantidad == 4 && marca != "FelipeLamparas"){ 
+        descuento = (cantidad * precio) * 0.20;
+        preciofinal = precio - descuento;
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad == 3 && marca == "ArgentinaLuz"){
+        descuento = (cantidad * precio) * 0.15;
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad == 3 && marca == "FelipeLamparas"){
+        descuento = (cantidad * precio) * 0.10;
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad == 3 && marca != "FelipeLamparas" || cantidad == 3 && marca != "ArgentinaLuz"){
+        descuento = (cantidad * precio) * 0.05;
+        preciofinal = (cantidad * precio) - descuento;
+    }
+else if (cantidad > 3)
+        preciofinal = (cantidad * precio);
+
+if (preciofinal < 120){
+    
+}
+
+document.getElementById("precioDescuento").value = preciofinal;
 }
